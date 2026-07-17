6a8d3f6bb97d4c721f31bbc8ac843a66f9f51774
Mohammed Swalih N K <swalink555@gmail.com>
2026-07-17 21:08:10 +0530

    refactor: Remove old settings and initialize new configuration structure

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     "apps.jobs.apps.JobsConfig",
     "apps.dashboard.apps.DashboardConfig",
 ]
