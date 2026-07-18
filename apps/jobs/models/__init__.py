2b7c5e09e6bb5a2f4f27732f38391de10b9ae7a2
Mohammed Swalih N K <swalink555@gmail.com>
2026-07-18 23:30:17 +0530

    feat: Add JobEvent model to track job-related events and statuses

--- apps/jobs/models/__init__.py
+++ apps/jobs/models/__init__.py
@@ -2,5 +2,7 @@ from .job import Job
 from .application import Application
 from .skill_log import SkillLog
 from .daily_stats import DailyStats
+from .raw_job import RawJob
+from .job_event import JobEvent
 
-__all__ = ["Job", "Application", "SkillLog", "DailyStats"]
+__all__ = ["Job", "Application", "SkillLog", "DailyStats", "RawJob", "JobEvent"]
