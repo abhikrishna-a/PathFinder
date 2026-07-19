7605325be9b5a678fe80c3ef1f5591859a0a73ea
Mohammed Swalih N K <swalink555@gmail.com>
2026-07-19 14:17:58 +0530

    feat: Add CredStore model for managing encrypted credentials and resume files

--- apps/jobs/models/__init__.py
+++ apps/jobs/models/__init__.py
@@ -4,5 +4,6 @@ from .skill_log import SkillLog
 from .daily_stats import DailyStats
 from .raw_job import RawJob
 from .job_event import JobEvent
+from .cred_store import CredStore
 
-__all__ = ["Job", "Application", "SkillLog", "DailyStats", "RawJob", "JobEvent"]
+__all__ = ["Job", "Application", "SkillLog", "DailyStats", "RawJob", "JobEvent", "CredStore"]
