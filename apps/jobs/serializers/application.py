910ab9b774afed1983f8ec9c4827e64d945fd05c
Mohammed Swalih N K <swalink555@gmail.com>
2026-07-20 12:35:11 +0530

    feat: Add resume attachment status and failed application flag to serializers

--- apps/jobs/serializers/application.py
+++ apps/jobs/serializers/application.py
@@ -15,3 +15,10 @@ class ApplicationSerializer(serializers.ModelSerializer):
             "skills_highlighted", "skills_in_job_desc", "skill_match_pct",
             "criteria_data", "skill_gaps", "match_explanation",
         ]
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        from apps.jobs.models import CredStore
+        if "criteria_data" in rep and isinstance(rep["criteria_data"], dict):
+            rep["criteria_data"]["resume_attached"] = CredStore.load().has_resume
+        return rep
